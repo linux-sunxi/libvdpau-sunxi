a4f40e2494b339be66e7f47b064f170c57310e96
Jens Kuske <jenskuske@gmail.com>
2015-07-17 17:11:26 +0200

    Use predefined types for vdp_* functions
    This also makes the compiler generate errors if we mess up the
    function signature.

--- vdpau_private.h
+++ vdpau_private.h
@@ -26,6 +26,7 @@
 
 #include <stdlib.h>
 #include <vdpau/vdpau.h>
+#include <vdpau/vdpau_x11.h>
 #include <X11/Xlib.h>
 
 #define INTERNAL_YCBCR_FORMAT (VdpYCbCrFormat)0xffff
@@ -174,76 +175,75 @@ void *handle_create(size_t size, VdpHandle *handle);
 void *handle_get(VdpHandle handle);
 void handle_destroy(VdpHandle handle);
 
-EXPORT VdpStatus vdp_imp_device_create_x11(Display *display, int screen, VdpDevice *device, VdpGetProcAddress **get_proc_address);
-VdpStatus vdp_device_destroy(VdpDevice device);
-VdpStatus vdp_preemption_callback_register(VdpDevice device, VdpPreemptionCallback callback, void *context);
+EXPORT VdpDeviceCreateX11 vdp_imp_device_create_x11;
+VdpDeviceDestroy vdp_device_destroy;
+VdpPreemptionCallbackRegister vdp_preemption_callback_register;
 
-VdpStatus vdp_get_proc_address(VdpDevice device, VdpFuncId function_id, void **function_pointer);
+VdpGetProcAddress vdp_get_proc_address;
 
-char const *vdp_get_error_string(VdpStatus status);
-VdpStatus vdp_get_api_version(uint32_t *api_version);
-VdpStatus vdp_get_information_string(char const **information_string);
+VdpGetErrorString vdp_get_error_string;
+VdpGetApiVersion vdp_get_api_version;
+VdpGetInformationString vdp_get_information_string;
 
+VdpPresentationQueueTargetCreateX11 vdp_presentation_queue_target_create_x11;
+VdpPresentationQueueTargetDestroy vdp_presentation_queue_target_destroy;
+VdpPresentationQueueCreate vdp_presentation_queue_create;
+VdpPresentationQueueDestroy vdp_presentation_queue_destroy;
+VdpPresentationQueueSetBackgroundColor vdp_presentation_queue_set_background_color;
+VdpPresentationQueueGetBackgroundColor vdp_presentation_queue_get_background_color;
+VdpPresentationQueueGetTime vdp_presentation_queue_get_time;
+VdpPresentationQueueDisplay vdp_presentation_queue_display;
+VdpPresentationQueueBlockUntilSurfaceIdle vdp_presentation_queue_block_until_surface_idle;
+VdpPresentationQueueQuerySurfaceStatus vdp_presentation_queue_query_surface_status;
 
-VdpStatus vdp_presentation_queue_target_create_x11(VdpDevice device, Drawable drawable, VdpPresentationQueueTarget *target);
-VdpStatus vdp_presentation_queue_target_destroy(VdpPresentationQueueTarget presentation_queue_target);
-VdpStatus vdp_presentation_queue_create(VdpDevice device, VdpPresentationQueueTarget presentation_queue_target, VdpPresentationQueue *presentation_queue);
-VdpStatus vdp_presentation_queue_destroy(VdpPresentationQueue presentation_queue);
-VdpStatus vdp_presentation_queue_set_background_color(VdpPresentationQueue presentation_queue, VdpColor *const background_color);
-VdpStatus vdp_presentation_queue_get_background_color(VdpPresentationQueue presentation_queue, VdpColor *const background_color);
-VdpStatus vdp_presentation_queue_get_time(VdpPresentationQueue presentation_queue, VdpTime *current_time);
-VdpStatus vdp_presentation_queue_display(VdpPresentationQueue presentation_queue, VdpOutputSurface surface, uint32_t clip_width, uint32_t clip_height, VdpTime earliest_presentation_time);
-VdpStatus vdp_presentation_queue_block_until_surface_idle(VdpPresentationQueue presentation_queue, VdpOutputSurface surface, VdpTime *first_presentation_time);
-VdpStatus vdp_presentation_queue_query_surface_status(VdpPresentationQueue presentation_queue, VdpOutputSurface surface, VdpPresentationQueueStatus *status, VdpTime *first_presentation_time);
+VdpVideoSurfaceCreate vdp_video_surface_create;
+VdpVideoSurfaceDestroy vdp_video_surface_destroy;
+VdpVideoSurfaceGetParameters vdp_video_surface_get_parameters;
+VdpVideoSurfaceGetBitsYCbCr vdp_video_surface_get_bits_y_cb_cr;
+VdpVideoSurfacePutBitsYCbCr vdp_video_surface_put_bits_y_cb_cr;
+VdpVideoSurfaceQueryCapabilities vdp_video_surface_query_capabilities;
+VdpVideoSurfaceQueryGetPutBitsYCbCrCapabilities vdp_video_surface_query_get_put_bits_y_cb_cr_capabilities;
 
-VdpStatus vdp_video_surface_create(VdpDevice device, VdpChromaType chroma_type, uint32_t width, uint32_t height, VdpVideoSurface *surface);
-VdpStatus vdp_video_surface_destroy(VdpVideoSurface surface);
-VdpStatus vdp_video_surface_get_parameters(VdpVideoSurface surface, VdpChromaType *chroma_type, uint32_t *width, uint32_t *height);
-VdpStatus vdp_video_surface_get_bits_y_cb_cr(VdpVideoSurface surface, VdpYCbCrFormat destination_ycbcr_format, void *const *destination_data, uint32_t const *destination_pitches);
-VdpStatus vdp_video_surface_put_bits_y_cb_cr(VdpVideoSurface surface, VdpYCbCrFormat source_ycbcr_format, void const *const *source_data, uint32_t const *source_pitches);
-VdpStatus vdp_video_surface_query_capabilities(VdpDevice device, VdpChromaType surface_chroma_type, VdpBool *is_supported, uint32_t *max_width, uint32_t *max_height);
-VdpStatus vdp_video_surface_query_get_put_bits_y_cb_cr_capabilities(VdpDevice device, VdpChromaType surface_chroma_type, VdpYCbCrFormat bits_ycbcr_format, VdpBool *is_supported);
+VdpOutputSurfaceCreate vdp_output_surface_create;
+VdpOutputSurfaceDestroy vdp_output_surface_destroy;
+VdpOutputSurfaceGetParameters vdp_output_surface_get_parameters;
+VdpOutputSurfaceGetBitsNative vdp_output_surface_get_bits_native;
+VdpOutputSurfacePutBitsNative vdp_output_surface_put_bits_native;
+VdpOutputSurfacePutBitsIndexed vdp_output_surface_put_bits_indexed;
+VdpOutputSurfacePutBitsYCbCr vdp_output_surface_put_bits_y_cb_cr;
+VdpOutputSurfaceRenderOutputSurface vdp_output_surface_render_output_surface;
+VdpOutputSurfaceRenderBitmapSurface vdp_output_surface_render_bitmap_surface;
+VdpOutputSurfaceQueryCapabilities vdp_output_surface_query_capabilities;
+VdpOutputSurfaceQueryGetPutBitsNativeCapabilities vdp_output_surface_query_get_put_bits_native_capabilities;
+VdpOutputSurfaceQueryPutBitsIndexedCapabilities vdp_output_surface_query_put_bits_indexed_capabilities;
+VdpOutputSurfaceQueryPutBitsYCbCrCapabilities vdp_output_surface_query_put_bits_y_cb_cr_capabilities;
 
-VdpStatus vdp_output_surface_create(VdpDevice device, VdpRGBAFormat rgba_format, uint32_t width, uint32_t height, VdpOutputSurface  *surface);
-VdpStatus vdp_output_surface_destroy(VdpOutputSurface surface);
-VdpStatus vdp_output_surface_get_parameters(VdpOutputSurface surface, VdpRGBAFormat *rgba_format, uint32_t *width, uint32_t *height);
-VdpStatus vdp_output_surface_get_bits_native(VdpOutputSurface surface, VdpRect const *source_rect, void *const *destination_data, uint32_t const *destination_pitches);
-VdpStatus vdp_output_surface_put_bits_native(VdpOutputSurface surface, void const *const *source_data, uint32_t const *source_pitches, VdpRect const *destination_rect);
-VdpStatus vdp_output_surface_put_bits_indexed(VdpOutputSurface surface, VdpIndexedFormat source_indexed_format, void const *const *source_data, uint32_t const *source_pitch, VdpRect const *destination_rect, VdpColorTableFormat color_table_format, void const *color_table);
-VdpStatus vdp_output_surface_put_bits_y_cb_cr(VdpOutputSurface surface, VdpYCbCrFormat source_ycbcr_format, void const *const *source_data, uint32_t const *source_pitches, VdpRect const *destination_rect, VdpCSCMatrix const *csc_matrix);
-VdpStatus vdp_output_surface_render_output_surface(VdpOutputSurface destination_surface, VdpRect const *destination_rect, VdpOutputSurface source_surface, VdpRect const *source_rect, VdpColor const *colors, VdpOutputSurfaceRenderBlendState const *blend_state, uint32_t flags);
-VdpStatus vdp_output_surface_render_bitmap_surface(VdpOutputSurface destination_surface, VdpRect const *destination_rect, VdpBitmapSurface source_surface, VdpRect const *source_rect, VdpColor const *colors, VdpOutputSurfaceRenderBlendState const *blend_state, uint32_t flags);
-VdpStatus vdp_output_surface_query_capabilities(VdpDevice device, VdpRGBAFormat surface_rgba_format, VdpBool *is_supported, uint32_t *max_width, uint32_t *max_height);
-VdpStatus vdp_output_surface_query_get_put_bits_native_capabilities(VdpDevice device, VdpRGBAFormat surface_rgba_format, VdpBool *is_supported);
-VdpStatus vdp_output_surface_query_put_bits_indexed_capabilities(VdpDevice device, VdpRGBAFormat surface_rgba_format, VdpIndexedFormat bits_indexed_format, VdpColorTableFormat color_table_format, VdpBool *is_supported);
-VdpStatus vdp_output_surface_query_put_bits_y_cb_cr_capabilities(VdpDevice device, VdpRGBAFormat surface_rgba_format, VdpYCbCrFormat bits_ycbcr_format, VdpBool *is_supported);
+VdpVideoMixerCreate vdp_video_mixer_create;
+VdpVideoMixerDestroy vdp_video_mixer_destroy;
+VdpVideoMixerRender vdp_video_mixer_render;
+VdpVideoMixerGetFeatureSupport vdp_video_mixer_get_feature_support;
+VdpVideoMixerSetFeatureEnables vdp_video_mixer_set_feature_enables;
+VdpVideoMixerGetFeatureEnables vdp_video_mixer_get_feature_enables;
+VdpVideoMixerSetAttributeValues vdp_video_mixer_set_attribute_values;
+VdpVideoMixerGetParameterValues vdp_video_mixer_get_parameter_values;
+VdpVideoMixerGetAttributeValues vdp_video_mixer_get_attribute_values;
+VdpVideoMixerQueryFeatureSupport vdp_video_mixer_query_feature_support;
+VdpVideoMixerQueryParameterSupport vdp_video_mixer_query_parameter_support;
+VdpVideoMixerQueryParameterValueRange vdp_video_mixer_query_parameter_value_range;
+VdpVideoMixerQueryAttributeSupport vdp_video_mixer_query_attribute_support;
+VdpVideoMixerQueryAttributeValueRange vdp_video_mixer_query_attribute_value_range;
+VdpGenerateCSCMatrix vdp_generate_csc_matrix;
 
-VdpStatus vdp_video_mixer_create(VdpDevice device, uint32_t feature_count, VdpVideoMixerFeature const *features, uint32_t parameter_count, VdpVideoMixerParameter const *parameters, void const *const *parameter_values, VdpVideoMixer *mixer);
-VdpStatus vdp_video_mixer_destroy(VdpVideoMixer mixer);
-VdpStatus vdp_video_mixer_render(VdpVideoMixer mixer, VdpOutputSurface background_surface, VdpRect const *background_source_rect, VdpVideoMixerPictureStructure current_picture_structure, uint32_t video_surface_past_count, VdpVideoSurface const *video_surface_past, VdpVideoSurface video_surface_current, uint32_t video_surface_future_count, VdpVideoSurface const *video_surface_future, VdpRect const *video_source_rect, VdpOutputSurface destination_surface, VdpRect const *destination_rect, VdpRect const *destination_video_rect, uint32_t layer_count, VdpLayer const *layers);
-VdpStatus vdp_video_mixer_get_feature_support(VdpVideoMixer mixer, uint32_t feature_count, VdpVideoMixerFeature const *features, VdpBool *feature_supports);
-VdpStatus vdp_video_mixer_set_feature_enables(VdpVideoMixer mixer, uint32_t feature_count, VdpVideoMixerFeature const *features, VdpBool const *feature_enables);
-VdpStatus vdp_video_mixer_get_feature_enables(VdpVideoMixer mixer, uint32_t feature_count, VdpVideoMixerFeature const *features, VdpBool *feature_enables);
-VdpStatus vdp_video_mixer_set_attribute_values(VdpVideoMixer mixer, uint32_t attribute_count, VdpVideoMixerAttribute const *attributes, void const *const *attribute_values);
-VdpStatus vdp_video_mixer_get_parameter_values(VdpVideoMixer mixer, uint32_t parameter_count, VdpVideoMixerParameter const *parameters, void *const *parameter_values);
-VdpStatus vdp_video_mixer_get_attribute_values(VdpVideoMixer mixer, uint32_t attribute_count, VdpVideoMixerAttribute const *attributes, void *const *attribute_values);
-VdpStatus vdp_video_mixer_query_feature_support(VdpDevice device, VdpVideoMixerFeature feature, VdpBool *is_supported);
-VdpStatus vdp_video_mixer_query_parameter_support(VdpDevice device, VdpVideoMixerParameter parameter, VdpBool *is_supported);
-VdpStatus vdp_video_mixer_query_parameter_value_range(VdpDevice device, VdpVideoMixerParameter parameter, void *min_value, void *max_value);
-VdpStatus vdp_video_mixer_query_attribute_support(VdpDevice device, VdpVideoMixerAttribute attribute, VdpBool *is_supported);
-VdpStatus vdp_video_mixer_query_attribute_value_range(VdpDevice device, VdpVideoMixerAttribute attribute, void *min_value, void *max_value);
-VdpStatus vdp_generate_csc_matrix(VdpProcamp *procamp, VdpColorStandard standard, VdpCSCMatrix *csc_matrix);
+VdpDecoderCreate vdp_decoder_create;
+VdpDecoderDestroy vdp_decoder_destroy;
+VdpDecoderGetParameters vdp_decoder_get_parameters;
+VdpDecoderRender vdp_decoder_render;
+VdpDecoderQueryCapabilities vdp_decoder_query_capabilities;
 
-VdpStatus vdp_decoder_create(VdpDevice device, VdpDecoderProfile profile, uint32_t width, uint32_t height, uint32_t max_references, VdpDecoder *decoder);
-VdpStatus vdp_decoder_destroy(VdpDecoder decoder);
-VdpStatus vdp_decoder_get_parameters(VdpDecoder decoder, VdpDecoderProfile *profile, uint32_t *width, uint32_t *height);
-VdpStatus vdp_decoder_render(VdpDecoder decoder, VdpVideoSurface target, VdpPictureInfo const *picture_info, uint32_t bitstream_buffer_count, VdpBitstreamBuffer const *bitstream_buffers);
-VdpStatus vdp_decoder_query_capabilities(VdpDevice device, VdpDecoderProfile profile, VdpBool *is_supported, uint32_t *max_level, uint32_t *max_macroblocks, uint32_t *max_width, uint32_t *max_height);
-
-VdpStatus vdp_bitmap_surface_create(VdpDevice device, VdpRGBAFormat rgba_format, uint32_t width, uint32_t height, VdpBool frequently_accessed, VdpBitmapSurface *surface);
-VdpStatus vdp_bitmap_surface_destroy(VdpBitmapSurface surface);
-VdpStatus vdp_bitmap_surface_get_parameters(VdpBitmapSurface surface, VdpRGBAFormat *rgba_format, uint32_t *width, uint32_t *height, VdpBool *frequently_accessed);
-VdpStatus vdp_bitmap_surface_put_bits_native(VdpBitmapSurface surface, void const *const *source_data, uint32_t const *source_pitches, VdpRect const *destination_rect);
-VdpStatus vdp_bitmap_surface_query_capabilities(VdpDevice device, VdpRGBAFormat surface_rgba_format, VdpBool *is_supported, uint32_t *max_width, uint32_t *max_height);
+VdpBitmapSurfaceCreate vdp_bitmap_surface_create;
+VdpBitmapSurfaceDestroy vdp_bitmap_surface_destroy;
+VdpBitmapSurfaceGetParameters vdp_bitmap_surface_get_parameters;
+VdpBitmapSurfacePutBitsNative vdp_bitmap_surface_put_bits_native;
+VdpBitmapSurfaceQueryCapabilities vdp_bitmap_surface_query_capabilities;
 
 #endif
