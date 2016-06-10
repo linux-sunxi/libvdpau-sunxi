a5abf97409913553a1c715d7f70998db952f6385
Andreas Baierl <ichgeh@imkreisrum.de>
2016-06-10 15:32:30 +0200

    fixup! Add CPU fallback for G2D

--- rgba_pixman.c
+++ rgba_pixman.c
@@ -93,7 +93,7 @@ VdpStatus vdp_pixman_blit(rgba_surface_t *rgba_dst, const VdpRect *dst_rect,
 
 	/* Composite to the dest_img */
 	pixman_image_composite32(
-		PIXMAN_OP_OVER, src, NULL, dst,
+		PIXMAN_OP_SRC, src, NULL, dst,
 		(src_rect->x0 * fscale_x), (src_rect->y0 * fscale_y),
 		0, 0,
 		dst_rect->x0, dst_rect->y0,
